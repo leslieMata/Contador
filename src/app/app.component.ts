1a50db34f563a535d1729a0524e73489b08cbb1c
leslieMata <95949303+leslieMata@users.noreply.github.com>
2023-03-23 12:09:03 -0600

    contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'contador';
+    public titulo : string = 'Contador';
+    public numero : number = 3;
+    public base : number = 3;
+  //   public sumar() : void {
+  //       this.numero += 1;
+  //   }
+  //   public restar() : void{
+  //       this.numero -= 1;
+  //   }
+  public contador(valor : number) : void{
+    this.numero += valor;
+} 
 }
